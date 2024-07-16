86721eeea256c39572d1a5e1f776019f5e2f35ae
Olha Rypich <olha.rypich@gmail.com>
2024-07-16 15:55:05 +0300

    make build

---
+++ index.6f593343.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top=(e.clientHeight-t.clientHeight)/2+"px",t.style.left=(e.clientWidth-t.clientWidth)/2+"px";
+//# sourceMappingURL=index.6f593343.js.map
